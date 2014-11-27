d40317e1dca6a84c9aaef8a60b7e3a2205ca1087
hepa <szabolcs.remenyi@gmail.com>
2014-11-27 18:15:06 +0100

    Init commit.

---
+++ src/basex/QueryExample.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basex;
+
+import static basex.RunQueries.query;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBException;
+import model.Mark;
+import model.Student;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.XQuery;
+import org.basex.query.value.node.ANode;
+import util.JAXBUtil;
+
+/**
+ * This example shows how queries can be executed in an iterative manner.
+ * Iterative evaluation will be slower, as more server requests are performed.
+ * 
+* This example requires a running database server instance. Documentation:
+ * http://docs.basex.org/wiki/Clients
+ * 
+* @author BaseX Team 2005-14, BSD License
+ */
+public final class QueryExample {
+
+    /**
+     * Main method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
+        // print exception
+    
+
+    
+        try {
+// create session
+            BaseXClient session = new BaseXClient("localhost", 1984, "admin", "admin");
+        try {
+// create query instance
+            final String input = "for $x in doc('rendszer')/json/diakok/diak return $x";
+            final BaseXClient.Query query = session.query(input);
+// loop through all results
+            while (query.more()) {
+                String xml = query.next();
+System.out.println(xml);
+                try {
+                    Student s = JAXBUtil.fromXMLElement(Student.class, xml);
+                    System.out.println(s);
+                } catch (JAXBException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+String jegy = "<jegy id=\"1\">\n" +
+"      <tantargy>K001</tantargy>\n" +
+"      <tanev>2014/2015</tanev>\n" +
+"      <erdemjegy>5</erdemjegy>\n" +
+"    </jegy>";
+
+                try {
+                    Mark m = JAXBUtil.fromXMLElement(Mark.class, jegy);
+                    System.out.println(m);
+                } catch (JAXBException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            
+            // print query info
+            System.out.println(query.info());
+// close query instance
+            query.close();
+
+          
+
+        } catch (final IOException ex) {
+// print exception
+            ex.printStackTrace();
+        }
+// close session
+        session.close();
+    }
+    catch (final IOException ex
+
+    
+        ) {
+// print exception
+            ex.printStackTrace();
+    }
+}
+}
---
+++ src/model/Teacher.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author zsolti
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="tanar")
+public class Teacher extends Person{
+    
+    @XmlAttribute
+    private int id;
+    
+    @XmlElement(name="osztalya")
+    private Class form;
+            
+            
+}
